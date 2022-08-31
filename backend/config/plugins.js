6385c6c1888fbd65d07e75e9cf196ac99357c095
Chaipitch Wongwangpaisarn <chaipitch6@gmail.com>
2022-08-31 16:20:19 +0700

    first commit

---
+++ backend/config/plugins.js
@@ -0,0 +1,17 @@
+/*eslint linebreak-style: ["error", "windows"]*/
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
